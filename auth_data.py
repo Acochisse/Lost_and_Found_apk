cf2d4aea70170c9c0307d735b58933245eb04518
Acochisse <1819@holbertonschool.com>
2022-01-21 17:08:17 -0600

    adding auth check, the browser run, and the screenshot generator

---
+++ auth_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+import json
+
+def auth_check():
+    try:
+        with open('auth_data.json', 'r') as fp:
+            data = json.load(fp)
+            print("auth_data found.")
+    except:
+        if not sys.argv[1]:
+            print("Holberton Username is missing")
+        if not sys.argv[2]:
+            print("Holberton Password is missing")
+        else:
+            intra_user = sys.argv[1]
+            intra_pass = sys.argv[2]
+            auth = {"intra_user_key": intra_user, "intra_pass_key": intra_pass, 'API_key': 'Db55801b283adfd75532275e6c1b8b28='}
+            with open('auth_data.json', 'w') as outfile:
+                json.dump(auth, outfile)

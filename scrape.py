5f0cf9c4e107f90d950194d1fa148649fc8c2f8c
Acochisse <1819@holbertonschool.com>
2022-01-22 16:06:31 -0600

    working version of the scraper. Logs in successfully and navigates to calendar

--- scrape.py
+++ scrape.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python3
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import os
-from pathlib import Path
+import sys
+import json
+import time
 
 #cwd = os.path.dirname(os.path.abspath(chromedriver))
-driver = webdriver.Chrome(executable_path='/home/ac0chisse/Holberton/Projects/Lost_and_Found_apk/chromedriver')
+#driver = webdriver.Chrome(executable_path='/home/ac0chisse/Holberton/Projects/Lost_and_Found-apk/chromedriver')
+firefox_options = webdriver.FirefoxOptions()
+driver = webdriver.Firefox()
 
-def load_auth():
-    super_path = os.path.dirname(os.path.abspath(__file__))
+def login_form_entry():
+    #super_path = os.path.dirname(os.path.abspath(__file__))
     try:
-        with open("{}/auth_data.json".format(super_path.rsplit("/", 1)[0]), "r") as json_file:
-            return json.load(json_file)
+         with open("auth_data.json", 'r') as fp:
+            jsondata = json.load(fp)
+            print("auth_data found.")
     except IOError:
         print("Authorization data not found")
         sys.exit()
 
-def login_form_entry():
-    driver.get('https://intranet.hbtn.io/auth/sign_in')
-    user = self.json_data["intra_user_key"]
-    pwd = self.json_data["intra_pass_key"]
-    u = driver.find_element_by_name('user[login]')
-    u.driver.send_keys(user)
-    p = driver.find_element_by_name('user[password')
-    p.driver.send_keys(pwd)
-    p.driver.send_keys(keys.RETURN)
+    driver.maximize_window
+    user = jsondata['intra_user_key']
+    pwd = jsondata['intra_pass_key']
+    address = "https://intranet.hbtn.io/auth/sign_in"
+    driver.get(address)
+    u = driver.find_element_by_name("user[login]")
+    u.send_keys(user)
+    p = driver.find_element_by_name("user[password]")
+    p.send_keys(pwd)
+    p.send_keys(Keys.RETURN)
+    time.sleep(6)
     
-    sleep(60)
-    driver.close
+    #cal = driver.get("https://intranet.hbtn.io/dashboards/my_planning")
+    driver.get("https://intranet.hbtn.io/dashboards/my_planning")
+    #cal.send_keys(Keys.DOWN)
+    #cal.send_keys(Keys.DOWN)
+    
 
 if __name__ == '__main__':
-    scrape()
+    login_form_entry()
